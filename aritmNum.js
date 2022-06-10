2ff5856256f2ca99b720d1253ab5ad30d08f0609
patrick993 <patrick.alvarado993@gmail.com>
2022-06-10 17:53:44 -0400

    Carga de proyecto

---
+++ aritmNum.js
@@ -0,0 +1,58 @@
+let numA = document.getElementById("numA");
+let numB = document.getElementById("numB");
+let resultado = document.getElementById("resultado")
+var html = ''
+
+resultado.onclick = function () {
+  momentoActual = new Date()
+  hora = momentoActual.getHours()
+  minuto = momentoActual.getMinutes()
+  segundo = momentoActual.getSeconds()
+  horaImprimible = hora + " : " + minuto + " : " + segundo
+
+  var res = parseInt(numA.value) + parseInt(numB.value)
+  var res1 = parseInt(numA.value) - parseInt(numB.value)
+  var res2 = parseInt(numA.value) * parseInt(numB.value)
+  var res3 = parseInt(numA.value) / parseInt(numB.value)
+  console.log("Suma: ", res, " -  Resta: ", res1, " - Mult: ", res2, " - Div:", res3)
+
+  html += '<br><div class=card>'
+  html += '<div class="card-header" name="form_reloj">' + horaImprimible + '</div>'
+  html += '<div class="card-body" style="width: 18rem;"><p> Suma: ' + res + '</p> <p> Resta: ' + res1 + '</p> Mult: ' + res2 + '</p> <p>Div: ' + res3 + '</p></div>'
+  html += '</div>'
+
+  if (html.length != 0) {
+    $('#contRes').html(html)
+  }
+  
+}
+function valideKey(evt) {
+  // code is the decimal ASCII representation of the pressed key.
+  var code = (evt.which) ? evt.which : evt.keyCode;
+
+  if (code == 8) { // backspace.
+    return true;
+  } else if (code >= 48 && code <= 57) { // is a number.
+    return true;
+  } else { // other keys.
+    return false;
+  }
+}
+
+// function mueveReloj(){
+//   momentoActual = new Date()
+//   hora = momentoActual.getHours()
+//   minuto = momentoActual.getMinutes()
+//   segundo = momentoActual.getSeconds()
+
+//   horaImprimible = hora + " : " + minuto + " : " + segundo
+
+//   document.form_reloj.reloj.value = horaImprimible
+
+//   setTimeout("mueveReloj()",1000)
+// }
+
+
+
+
+
